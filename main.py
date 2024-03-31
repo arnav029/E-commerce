033c724bd35000545433d9e6adc600ae9cff52ba
Arnav <arnavnpatil@gmail.com>
2024-03-31 11:50:40 +0530

    Added authentication.py

--- main.py
+++ main.py
@@ -2,8 +2,45 @@ from fastapi import FastAPI
 from tortoise import models
 from tortoise.contrib.fastapi import register_tortoise
 from models import *
+from authentication import get_hashed_password
+from tortoise.signals import  post_save
+from tortoise import List,Optional, Type
+from tortoise import BaseDBAsyncClient
+from passlib.context import CryptContext
+
 
 app = FastAPI()
+
+
+@post_save(User)
+async def create_business(
+        sender:"Type[User]",
+        instance: User,
+        created: bool,
+        using_db: "Optional[BaseDBAsyncClient]",
+        update_fields: List[str]
+) -> None:
+    if created:
+        business_obj = await Business.create(
+            business_name = instance.username , owner = instance
+        )
+
+        await business_pydantic.from_tortoise_orm(business_obj)
+
+
+
+
+
+@app.post("/registration")
+async def user_registraion(user: user_pydantic):
+    user_info = user.dict(exclude_unset=True)
+    user_info["password"] = get_hashed_password(user_info["password"])
+    user_obj = await User.create(**user_info)
+    new_user = await user_pydantic.from_tortoise_orm(user_obj)
+    return {
+        "status": "ok",
+        "data" : f"Welcome {new_user.username}. Please check your email inbox."
+    }
 
 @app.get("/")
 def root():
